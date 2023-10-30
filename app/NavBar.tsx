d972207f165df9afac3c6ccdc8a02ac02327c9bd
brtkn <haha>
2023-10-30 14:44:07 +0300

    Style the active link and the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,14 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 import { BsFillBugFill } from "react-icons/bs";
+import { usePathname } from "next/navigation";
+import classNames from "classnames";
 
 const NavBar = () => {
+  const pathName = usePathname();
+  console.log(pathName);
   const links = [
     {
       label: "Dashboard",
@@ -15,15 +21,24 @@ const NavBar = () => {
   ];
 
   return (
-    <nav className="flex space-x-6 border-b-2 py-9 items-center px-5">
+    <nav className="flex space-x-6 border-b-2 py-8 items-center px-6">
       <Link href="/">
         <BsFillBugFill />
       </Link>
-      <ul className="flex space-x-6">
-        <li className="text-zinc-500 hover:text-zinc-800 transition-colors">
-          Dashboard
-        </li>
-        <li>Issues</li>
+      <ul className="flex space-x-8">
+        {links.map((link) => (
+          <li
+            className={classNames({
+              "text-zinc-900": link.href === pathName,
+              "text-zinc-500": link.href !== pathName,
+              "hover:text-zinc-800 transition-colors text-xl items-center":
+                true,
+            })}
+            key={link.href}
+          >
+            <Link href={link.href}>{link.label}</Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
